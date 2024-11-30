20e6c790ccc4a53ce90596a9a49771e0cbea542f
Mohamed Dahani <mdahani@e2r11p11.1337.ma>
2024-11-30 18:24:47 +0100

    fix some bugs

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 14:21:09 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/30 17:24:12 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/30 18:24:21 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@
 #include <stdarg.h>
 # include <unistd.h>
 
-int				ft_printf(const char *string, ...);
-int				ft_putchar(char c);
-int				ft_putstr(char *str);
-int				ft_putnbr(int nbr);
-unsigned long	convert_hexa(unsigned long nbr);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_printf(const char *string, ...);
+int	ft_putnbr(int nbr);
+int	convert_hexa(unsigned long nbr);
+int	convert_decimal(unsigned int nbr);
 
 #endif
