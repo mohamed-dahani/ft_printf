1157307350f0afd8746a9811e4e331d3976a54e8
Mohamed Dahani <mdahani@e2r11p11.1337.ma>
2024-11-30 19:37:52 +0100

    add some tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 14:25:57 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/30 18:25:37 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/30 19:34:51 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int main(void)
 	char c = 'a';
 	char *str = "mohamed";
 	int nbr = 2000;
-	int dec = -123;
+	int dec = 123;
+	int hexa = 2024;
 	ft_printf("%c\n", c);
 	ft_printf("%s\n", str);
 	ft_printf("%d\n", nbr);
-	ft_printf("%p\n", &str);
+	ft_printf("%p\n", &str);;
 	ft_printf("%u\n", dec);
-
-
-	printf("%u", dec);
+	ft_printf("%x\n", hexa);
+	ft_printf("%X\n", hexa);
 }
