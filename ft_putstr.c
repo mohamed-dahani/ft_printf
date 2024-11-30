613b6a1089e0e58c00dbd4db6d95cf386c2c5d34
Mohamed Dahani <mdahani@e2r11p11.1337.ma>
2024-11-30 16:55:26 +0100

    handle the null

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 15:14:59 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/30 14:28:28 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/30 16:50:12 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int	ft_putstr(char *str)
 	int	i;
 
 	i = 0;
+	if (str == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
 	while (str[i])
 	{
 		ft_putchar(str[i]);
