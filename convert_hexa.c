17bb22f881e62f126f80e909ebf1a26d342764ee
Mohamed Dahani <mdahani@e2r11p11.1337.ma>
2024-11-30 19:07:51 +0100

    i change convert_hexa to let her more dynamic like now she can return hexa_degits lower and upper case

--- convert_hexa.c
+++ convert_hexa.c
@@ -6,35 +6,39 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 16:57:46 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/30 18:07:36 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/30 19:04:05 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	generte_address(unsigned long nbr);
+static int	generte_address(unsigned long nbr, char type);
 
-int	convert_hexa(unsigned long nbr)
+int	convert_hexa(unsigned long nbr, char type)
 {
 	int	result;
 
 	result = 0;
 	result += ft_putstr("0x");
-	result += generte_address(nbr);
+	result += generte_address(nbr, type);
 	return (result);
 }
 
-static int	generte_address(unsigned long nbr)
+static int	generte_address(unsigned long nbr, char type)
 {
 	int		result;
 	char	*hexa_digits;
 
 	result = 0;
-	hexa_digits = "0123456789abcdef";
+	if (type == 'l')
+		hexa_digits = "0123456789abcdef";
+	else
+		hexa_digits = "0123456789ABCDEF";
+	
 	if (nbr >= 16)
 	{
-		result += generte_address(nbr / 16);
-		result += generte_address(nbr % 16);
+		result += generte_address(nbr / 16, type);
+		result += generte_address(nbr % 16, type);
 	}
 	else
 		result += ft_putchar(nbr[hexa_digits]);
