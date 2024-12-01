e53d8968871d1c45718759e0b3bb3d0b53eb9be0
Mohamed Dahani <mdahani@e2r11p11.1337.ma>
2024-12-01 18:44:01 +0100

    fix a norms

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 15:17:48 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/30 17:53:48 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/12/01 18:42:58 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_putnbr(int nbr)
 	if (nbr < 0)
 	{
 		result += write(1, "-", 1);
-		result += nbr = -nbr;
+		nbr = -nbr;
 	}
 	if (nbr >= 10)
 	{
